5190dfba306798430e221d30b81a44a930083dab
abbas2483 <sayedabbasraza38@gmail.com>
2026-05-01 00:51:46 +0530

    solving the build issues on railway

--- app/dashboard/projects/new/page.tsx
+++ app/dashboard/projects/new/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState } from 'react'
 import { createProject } from '@/lib/firebase/projects'
 import { useRouter } from 'next/navigation'
--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { space, radius, colors, shadows } from '@/lib/cssVars'
 
 import { useState, useEffect, memo } from 'react'
